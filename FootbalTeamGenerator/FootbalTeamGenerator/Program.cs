5d683910378e16eda19914f317420285309950ec
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-17 13:24:54 +0300

    <Committing>

---
+++ FootbalTeamGenerator/FootbalTeamGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FootbalTeamGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+            Console.WriteLine("123");
+        }
+    }
+}
